e888c851eb222fc0f6ae7436381fcc9e139b6294
MANISH <khandagalemanish21@gmail.com>
2026-01-07 00:20:11 +0530

    ui updated

--- src/app/excel-to-pdf/excel-to-pdf/excel-to-pdf.component.ts
+++ src/app/excel-to-pdf/excel-to-pdf/excel-to-pdf.component.ts
@@ -1,37 +1,55 @@
 import { Component } from '@angular/core';
 import * as XLSX from 'xlsx';
 import { PDFDocument, rgb } from 'pdf-lib';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-excel-to-pdf',
-  imports: [],
+  imports:[CommonModule],
   templateUrl: './excel-to-pdf.component.html',
 })
 export class ExcelToPdfComponent {
 
-  async onExcelUpload(event: Event) {
+  excelFile: File | null = null;
+  selectedFileName = '';
+
+  onFileSelect(event: Event) {
     const input = event.target as HTMLInputElement;
     if (!input.files?.length) return;
 
-    const file = input.files[0];
+    this.excelFile = input.files[0];
+    this.selectedFileName = this.excelFile.name;
+  }
+downloadSampleExcel() {
+  const url = 'assets/sample.xlsx';
 
-    // 1️⃣ Read Excel
-    const buffer = await file.arrayBuffer();
-    const wb = XLSX.read(buffer, { type: 'array' });
-    const sheet = wb.Sheets[wb.SheetNames[0]];
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'sample-excel-format.xlsx';
+  a.click();
+}
+
+
+  async generatePdf() {
+    if (!this.excelFile) return;
+
+    /* 1️⃣ Read Excel */
+    const buffer = await this.excelFile.arrayBuffer();
+    const workbook = XLSX.read(buffer, { type: 'array' });
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const rows: any[] = XLSX.utils.sheet_to_json(sheet);
 
     if (!rows.length) return;
 
     const fullName = `${rows[0].firstName} ${rows[0].lastName}`;
 
-    // 2️⃣ Load PDF
+    /* 2️⃣ Load PDF */
     const pdfBytes = await fetch('assets/tax.pdf').then(r => r.arrayBuffer());
     const pdfDoc = await PDFDocument.load(pdfBytes);
     const page = pdfDoc.getPages()[0];
 
-    // 3️⃣ Write text
-    page.drawRectangle({
+    /* 3️⃣ Write Text */
+     page.drawRectangle({
       x: 35, y: 592, width: 200, height: 12, color: rgb(1, 1, 1),
     });
     page.drawText(fullName, { x: 36, y: 595, size: 10 });
@@ -41,7 +59,7 @@ export class ExcelToPdfComponent {
     });
     page.drawText(fullName, { x: 36, y: 675, size: 10 });
 
-    // 4️⃣ Download
+    /* 4️⃣ Download */
     const bytes = await pdfDoc.save();
     const blob = new Blob([bytes], { type: 'application/pdf' });
     const url = URL.createObjectURL(blob);
@@ -52,5 +70,6 @@ export class ExcelToPdfComponent {
     a.click();
 
     URL.revokeObjectURL(url);
+
   }
 }
